9ffde6c4ceb8b403c1324fbe19d053d043535e17
tenney_olarh <alameenadetonma04@gmail.com>
2026-04-11 19:37:42 +0100

    button  hover corrected

--- Sezan-site/src/Components/Hero.jsx
+++ Sezan-site/src/Components/Hero.jsx
@@ -10,8 +10,8 @@ function Hero (){
              <h1 className="w-[308px] text-8xl h-[102.5px] font-normal leading-[96px] tracking-[4.8px]">Commerce</h1>
              <p className="w-[576px] h-[84px] text-xl tracking-[1px] leading-[1.2] text-[#64748B] mt-[20px] mb-[20px] font-normal">Experience the future of fluid shopping. We blend <br />arhitectural precision with liquid editorial  deign to curate <br />the world's most sought-after essentials  </p>
                <div className="flex flex-row gap-4">
-                <Link to = "./ "><button className="bg-gradient-to-r from-[#3132ED] to-[#5054FF] text-white  rounded-[8px]  w-[184px] h-[56px]         justify-center items-center hover:bg-red-500 transition duration-300 flex flex-row items-center gap-2 shadow-md"> <span>Shop Now</span><img src={arrow} alt="" /></button></Link>
-                <Link to = "./"><button className="bg-[#FFFFFF] text-[#3132ED] w-[184px] h-[56px] justify-center items-center font-medium hover:bg-red-500 transition duration-300 flex flex-row items-center gap-2 shadow-md"> <span>Browse site</span></button></Link>
+                <Link to = "./ "><button className="bg-gradient-to-r from-[#3132ED] to-[#5054FF] text-white  rounded-[8px]  w-[184px] h-[56px] justify-center items-center shadow-md hover:from-red-500 hover:to-red-600 transition-all duration-300  hover:scale-105 flex flex-row items-center gap-2 cursor-pointer"> <span>Shop Now</span><img src={arrow} alt="" /></button></Link>
+                <Link to = "./"><button className="bg-[#FFFFFF] text-[#3132ED] w-[184px] h-[56px] justify-center items-center font-medium shadow-md hover:bg-red-500 transition duration-300 flex flex-row items-center gap-2 cursor-pointer"> <span>Browse site</span></button></Link>
               </div>
             </section>
            <div>  <Link><img src={hero} alt="" className="object-cover w-[485px] h-[620px] mt-[30px]" /></Link></div> 
--- Sezan-site/src/Pages/Landing.jsx
+++ Sezan-site/src/Pages/Landing.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../Components/Navbar"
 import Hero from "../Components/Hero"   
+import Curated from "../Components/Landing/Curated"
 function Landing (){
     return(
         <>
         <Navbar/>
         <Hero/>
+        <Curated/>
         </>
     )
 }
