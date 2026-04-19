ff2d550eca05aa84c9288f992fb2e1c3842b02c2
tenney_olarh <alameenadetonma04@gmail.com>
2026-04-19 12:59:30 +0100

    adjusted space

--- Sezan-site/src/Components/Landing/Curated.jsx
+++ Sezan-site/src/Components/Landing/Curated.jsx
@@ -17,7 +17,7 @@ function Curated (){
                 <h1 className=" font-black text-[36px] text-[#312E81] leading-[40px] tracking-[0.9px]">Curated Collections</h1>
                 <Link to="/login" className=" font-bold text-[16px] text-[#3132ED] leading-[24px] flex flex-row gap-[8px] h-[24px] ">View All Categories <img src={arrow2} alt="Arrow"  className="object-cover w-[15px] h-[15px] cursor-pointer"/></Link>
             </div>
-            <div className="grid grid-cols-[1fr_1fr] gap-[24px] pr-[32px] pl-[32px] mb-[96px]  w-full h-full">
+            <div className="grid grid-cols-[1fr_1fr] gap-[24px] pr-[32px] pl-[32px] pb-[96px]">
                 <section><img src={images[0].src} alt={images[0].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/></section>
                 <section className="grid grid-rows-[1fr_1fr] gap-[24px] "> 
                     <img src={images[1].src} alt={images[1].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/>
