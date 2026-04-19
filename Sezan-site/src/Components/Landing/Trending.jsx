52c815d73601a4dc68a8a8b25c5a8cc904beba42
tenney_olarh <alameenadetonma04@gmail.com>
2026-04-19 13:34:10 +0100

    completed the centered header

--- Sezan-site/src/Components/Landing/Trending.jsx
+++ Sezan-site/src/Components/Landing/Trending.jsx
@@ -1,7 +1,8 @@
 function Trending () {
     return(
         <div>
-                <h1>Trending</h1>   
+            <p className="font-bold text-[12px] leading-[16px] tracking-[1.2px] text-[#006C46]  flex justify-center mt-[96px]">THE HOT LIST </p> 
+            <h1 className="font-black text-[48px] leading-[48px] tracking-[-2.4px] text-[#131B2E] flex justify-center    pt-[8.5px]">Trending Now</h1>
         </div>
     )
 }
