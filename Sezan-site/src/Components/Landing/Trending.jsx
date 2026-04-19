f48b3356451c30a6e492c939385f4fe57b1154e5
tenney_olarh <alameenadetonma04@gmail.com>
2026-04-19 12:46:17 +0100

    added trending .jsx coponent to the landing folder and  export  it

---
+++ Sezan-site/src/Components/Landing/Trending.jsx
@@ -0,0 +1,8 @@
+function Trending () {
+    return(
+        <div>
+                <h1>Trending</h1>   
+        </div>
+    )
+}
+export default Trending;
--- Sezan-site/src/Pages/Landing.jsx
+++ Sezan-site/src/Pages/Landing.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../Components/Navbar"
 import Hero from "../Components/Hero"   
 import Curated from "../Components/Landing/Curated"
+import Trending from "../Components/Landing/Trending";
 function Landing (){
     return(
         <>
         <Navbar/>
         <Hero/>
         <Curated/>
+        <Trending/>
         </>
     )
 }
