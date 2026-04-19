6e0a06983f7d147e1b9af6f6c09575601f40095c
tenney_olarh <alameenadetonma04@gmail.com>
2026-04-19 14:03:29 +0100

    replaced nominals of img

--- Sezan-site/src/Components/Landing/Trending.jsx
+++ Sezan-site/src/Components/Landing/Trending.jsx
@@ -1,4 +1,26 @@
+import wamage1 from "../../assets/watch.png"
+import shomage2 from "../../assets/sneakers.png"
+import glage2 from "../../assets/glass.png"
+import headage from "../../assets/headset.png"
 function Trending () {
+    const props =[
+        {
+            title : "Architect Chrono V1"
+            price : "$299.00"
+            img : wamage1
+        },
+        {
+            title:"Velocity Runner"
+            price:"$145.00"
+            img : shomage2
+        },
+        {
+            title: "Eclipse Shade Pro"
+            price :"$199.00"
+            img : glage2  
+        },
+        
+    ]
     return(
         <div>
             <p className="font-bold text-[12px] leading-[16px] tracking-[1.2px] text-[#006C46]  flex justify-center mt-[96px]">THE HOT LIST </p> 
