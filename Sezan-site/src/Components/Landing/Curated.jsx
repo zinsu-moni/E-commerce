c4db811d4ce61dc2357619fffa485e2285f0395f
tenney_olarh <alameenadetona04@gmail.com>
2026-04-20 11:01:46 +0100

    finished quote.jsx

--- Sezan-site/src/Components/Landing/Curated.jsx
+++ Sezan-site/src/Components/Landing/Curated.jsx
@@ -1,33 +1,63 @@
 import { Link } from "react-router-dom";
-import arrow2 from "../../assets/arrow2.png"
-import Electronics from "../../assets/Electronics.png"
-import Fashion from "../../assets/Fashion.png"
-import Home from "../../assets/Home.png"
-import Lifestyle from "../../assets/Lifestyle.png"
-function Curated (){
-    const images = [
-        { id: 0, src: Electronics, alt: "Electronics" },
-        { id: 1, src: Fashion, alt: "Fashion" },
-        { id: 2, src: Home, alt: "Home" },
-        { id: 3, src: Lifestyle, alt: "Lifestyle" },
-      ];
-    return( 
-        <div className="bg-[#F2F3FF]  size-auto ">
-            <div className="flex flex-row items-center justify-between pt-[96px] pr-[32px] pb-[96px] pl-[32px]">
-                <h1 className=" font-black text-[36px] text-[#312E81] leading-[40px] tracking-[0.9px]">Curated Collections</h1>
-                <Link to="/login" className=" font-bold text-[16px] text-[#3132ED] leading-[24px] flex flex-row gap-[8px] h-[24px] ">View All Categories <img src={arrow2} alt="Arrow"  className="object-cover w-[15px] h-[15px] cursor-pointer"/></Link>
-            </div>
-            <div className="grid grid-cols-[1fr_1fr] gap-[24px] pr-[32px] pl-[32px] pb-[96px]">
-                <section><img src={images[0].src} alt={images[0].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/></section>
-                <section className="grid grid-rows-[1fr_1fr] gap-[24px] "> 
-                    <img src={images[1].src} alt={images[1].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/>
-                    <div className="grid grid-cols-[1fr_1fr] gap-[24px]">
-                        <img src={images[2].src} alt={images[2].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/>
-                        <img src={images[3].src} alt={images[3].alt} className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"/>
-                    </div>
-                </section>
-            </div>
-        </div>
-    )
+import arrow2 from "../../assets/arrow2.png";
+import Electronics from "../../assets/Electronics.png";
+import Fashion from "../../assets/Fashion.png";
+import Home from "../../assets/Home.png";
+import Lifestyle from "../../assets/Lifestyle.png";
+function Curated() {
+  const images = [
+    { id: 0, src: Electronics, alt: "Electronics" },
+    { id: 1, src: Fashion, alt: "Fashion" },
+    { id: 2, src: Home, alt: "Home" },
+    { id: 3, src: Lifestyle, alt: "Lifestyle" },
+  ];
+  return (
+    <div className="bg-[#F2F3FF]  size-auto ">
+      <div className="flex flex-row items-center justify-between pt-[96px] pr-[32px] pb-[96px] pl-[32px]">
+        <h1 className=" font-black text-[36px] text-[#312E81] leading-[40px] tracking-[0.9px]">
+          Curated Collections
+        </h1>
+        <Link
+          to="/login"
+          className=" font-bold text-[16px] text-[#3132ED] leading-[24px] flex flex-row gap-[8px] h-[24px] "
+        >
+          View All Categories{" "}
+          <img
+            src={arrow2}
+            alt="Arrow"
+            className="object-cover w-[15px] h-[15px] cursor-pointer"
+          />
+        </Link>
+      </div>
+      <div className="grid grid-cols-[1fr_1fr] gap-[24px] pr-[32px] pl-[32px] pb-[96px]">
+        <section>
+          <img
+            src={images[0].src}
+            alt={images[0].alt}
+            className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"
+          />
+        </section>
+        <section className="grid grid-rows-[1fr_1fr] gap-[24px] ">
+          <img
+            src={images[1].src}
+            alt={images[1].alt}
+            className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"
+          />
+          <div className="grid grid-cols-[1fr_1fr] gap-[24px]">
+            <img
+              src={images[2].src}
+              alt={images[2].alt}
+              className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"
+            />
+            <img
+              src={images[3].src}
+              alt={images[3].alt}
+              className="object-cover cursor-pointer transition-transform duration-500 hover:-translate-y-[10px]"
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 export default Curated;
--- Sezan-site/src/Components/Landing/Quote.jsx
+++ Sezan-site/src/Components/Landing/Quote.jsx
@@ -1,8 +1,32 @@
-function Quote (){
-    return(
-        <div className="bg-[#EAEDFF]  h-[558px] py-[96px] px-[192px]">
-
+import icon from "../../assets/Comma.png";
+import quotter from "../../assets/Quotter.png";
+function Quote() {
+  return (
+    <div className="bg-[#EAEDFF]  h-[558px] py-[96px] px-[192px ] flex   flex-col items-center ">
+      <img src={icon} alt="Quote" className="w-[42.5px] h-[30px]" />
+      <p className="w-[866.8900146484375px] h-[200px] text-[36px] font-bold leading-[40px] italic text-[#131B2E] text-center mt-[40px] ">
+        "The shopping experience here feels like walking through a contemporary
+        art gallery. The attention to detail in both the interface and the
+        products they curate is unparalleled in modern e-commerce."
+      </p>
+      <div className="flex flex-row mt-[40px] gap-[24px] items-center">
+        <section>
+          <img
+            src={quotter}
+            alt="Quotter"
+            className="rounded-full border-[#3132ED] border-[2px] object-cover]"
+          />
+        </section>
+        <div className="flex flex-col">
+          <p className="font-black leading-[24px] text-[#131B2E] text-[18px]font- Inter">
+            Julian Vance
+          </p>
+          <p className="font-medium text-[14px] leading-[20px] text-[#434656] ">
+            Creative Director, NeoSpace
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Quote;
