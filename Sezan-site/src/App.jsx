7940ad6d57eb99456ef09091f89497347885fabf
tenney_olarh <alameenadetona04@gmail.com>
2026-05-19 13:23:45 +0100

    importation of images and signup pages

--- Sezan-site/src/App.jsx
+++ Sezan-site/src/App.jsx
@@ -3,13 +3,15 @@ import Landing from "./Pages/Landing"
 import ProductList from "./Pages/ProductList"
 import ProductDetail from "./Pages/ProductDetail"
 import CheckOut from "./Pages/CheckOut"
+import Signup from "./Pages/Signup"
 import Login from "./Pages/Login"
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Landing/>} />
-      <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       <Route path="/products" element={<ProductList />} />
       <Route path="/products/:id" element={<ProductDetail />} />
       <Route path="/checkout" element={<CheckOut />} />
---
+++ Sezan-site/src/Pages/Signup.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import logBG from "../assets/logBG.png";
+const Signup = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">Sign Up</h1>
+        </div>
+    );
+}
+export default Signup;
